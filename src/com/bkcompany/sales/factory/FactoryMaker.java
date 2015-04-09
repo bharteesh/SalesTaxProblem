6f4266e2bea262ad93a6f89df26c5ec8b8ba98f8
bharteesh <bharteesh@users.noreply.github.com>
2015-04-09 09:44:13 -0400

    Update FactoryMaker.java

--- src/com/bkcompany/sales/factory/FactoryMaker.java
+++ src/com/bkcompany/sales/factory/FactoryMaker.java
@@ -1,6 +1,11 @@
 package com.bkcompany.sales.factory;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.bkcompany.sales.Constants;
+import com.bkcompany.sales.exception.SalesTaxException;
 
 
 /**
@@ -10,20 +15,29 @@ import com.bkcompany.sales.Constants;
  *
  */
 public class FactoryMaker {
-	private static ProductFactory pf = null;
+	private static ProductFactory productFactory = null;
+	
+	// Map which holds product type as key and Concrete Product Factory as value
+	@SuppressWarnings({ "serial" })
+	private static final Map<String,ProductFactory> factoryMap =
+		    Collections.unmodifiableMap(new HashMap<String,ProductFactory>() {{
+		        put(Constants.BOOK, BookFactory.getInstance());
+		        put(Constants.CHOCOLATE, FoodFactory.getInstance());
+		        put(Constants.PILLS, MedicalFactory.getInstance());
+		        put(Constants.MISC, MiscellaneousFactory.getInstance());
+		    }});
 
-	public static ProductFactory getFactory(String choice) {
-		if (choice.equals(Constants.BOOK)) {
-			pf = BookFactory.getInstance();
-		} else if (choice.equals(Constants.CHOCOLATE)) {
-			pf = FoodFactory.getInstance();
-		} else if (choice.equals(Constants.PILLS)) {
-			pf = MedicalFactory.getInstance();
-		} else if (choice.equals(Constants.MISC)) {
-			pf = MiscellaneousFactory.getInstance();
-		} else {
-			pf = MiscellaneousFactory.getInstance();
-		}
-		return pf;
+	/**
+	 * 
+	 * @param productType
+	 * @return Concrete Product Factory instance for a given product type
+	 * @throws SalesTaxException
+	 */
+	public static ProductFactory getFactory(String productType) throws SalesTaxException{
+		productFactory = factoryMap.get(productType);
+		if (productFactory == null) {
+	        throw new SalesTaxException();
+	    }
+		return productFactory;
 	}
 }
